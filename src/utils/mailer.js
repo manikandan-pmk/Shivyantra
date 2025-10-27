e719adcdcb7d60e6f6fa27670d46581e771ac384
manikandan-pmk <manikandan47768@gmail.com>
2025-10-27 17:47:21 +0530

    nodemailer addes

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -3,8 +3,8 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  port: Number(process.env.EMAIL_PORT) || 587,
-  secure: false, // use true for port 465, false for others
+  port: Number(process.env.EMAIL_PORT) || 465,
+  secure: true, // use true for port 465, false for others
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
